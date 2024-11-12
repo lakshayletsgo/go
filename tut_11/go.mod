9760429dd7f4be8775a81e778ee3525f4968a23f
lakshay <lakshay22csu100@ncuindia.edu>
2024-11-13 01:30:43 +0530

    Pointers

---
+++ tut_11/go.mod
@@ -0,0 +1,3 @@
+module tut_11
+
+go 1.23.2
--- tut_11/main.go
+++ tut_11/main.go
@@ -1,3 +1,30 @@
+// Pointers
+// They are used to point to a variable without creating its copy
+// This is the syntax
+// var p *int
 package main
-import "fmt"
-func mai
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var p *int
+
+	// Or we can use this method
+	myString := "Lakshay"
+	myStringPr := &myString
+	fmt.Printf("%v\n", *myStringPr) //This will give the value of the mystring i.e. dereference the pointer
+	fmt.Printf("%v\n", &myStringPr) //This will give the address of the mystring
+
+	// We can dereference the pointer and then re assign the same pointer
+	var message *string
+	messageDe := "Ok"
+	message = &messageDe
+	messageDe = *message
+	messageDe = "hello"
+	*message = messageDe
+	fmt.Printf("%v\n", *message)
+
+	fmt.Println("Pointers in GO")
+}
