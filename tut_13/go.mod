65e1116800c950e0056fb3b7255529366db5be86
lakshay <lakshay22csu100@ncuindia.edu>
2024-11-16 15:15:43 +0530

    Using local packages in the local environment

--- tut_13/go.mod
+++ tut_13/go.mod
@@ -1,7 +1,13 @@
 module tut_13
-replace  sample v0.0.0 => ../sample
 
-require(
-    ../sample v0.0.0
+replace (
+	github.com/lakshayletsgo/go/sample v1.0.0 => ../sample
+	github.com/lakshayletsgo/go/sample2 v1.0.0 => ../sample2
 )
+
+require (
+	github.com/lakshayletsgo/go/sample v1.0.0
+	github.com/lakshayletsgo/go/sample2 v1.0.0
+)
+
 go 1.23.2
--- tut_13/main.go
+++ tut_13/main.go
@@ -2,9 +2,30 @@ package main
 
 import (
 	"fmt"
+
+	Sum "github.com/lakshayletsgo/go/sample2"
+	// If we have to import the package then we have to first upload it to github and then get it
+	// We have to initialize it also like we first need to write go mod init github.com/lakshayletsgo/go/sample in sample file command prompt and then push it
+	// Then we have to
+	// replace (
+	// 	github.com/lakshayletsgo/go/sample v1.0.0 => ../sample
+	// 	github.com/lakshayletsgo/go/sample2 v1.0.0 => ../sample2
+	// )
+
+	// require (
+	// 	github.com/lakshayletsgo/go/sample v1.0.0
+	// 	github.com/lakshayletsgo/go/sample2 v1.0.0
+	// )
+
+	// This is go.mod file of this folder and then we can use the package
+
+	"github.com/lakshayletsgo/go/sample"
 )
 
 func main() {
 
-	fmt.Println()
+	fmt.Println(Sum.Sum2(1, 2))
+	fmt.Print(Sum3.Sum(1, 2))
+	// fmt.Println(Sum3.Sum(1,2))
+
 }
